1352de0944e215bc32b226a3ab2c8a43ef97ff74
Grace Tan <gracetan37@gmail.com>
2019-02-06 08:11:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
   return object
 }
 function deleteFromObjectByKey(object,key) {
-  var newObj = Object.assign({},object,key)
+  var object = Object.assign({},object)
+  delete newObj.key
   return object;
 }
 function destructivelyDeleteFromObjectByKey(object,key) {
