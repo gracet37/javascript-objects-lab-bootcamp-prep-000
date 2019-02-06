4befa527d6880e4da915187970c7eed8eb31c7dd
Grace Tan <gracetan37@gmail.com>
2019-02-06 07:11:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
   object[key] = value;
   return object
 }
+var newRecipes = Object.assign({}, recipes)
 function deleteFromObjectByKey(object,key) {
-  
+  delete object.key;
+  return object;
 }
